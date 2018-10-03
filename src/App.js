731a42ceaacffd0d28687a96687b136ab34cc922
aronsonben <14benaronson@gmail.com>
2018-10-03 10:47:25 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import ReactTable from "react-table";
+import matchSorter from 'match-sorter';
+import './App.css';
+import 'react-table/react-table.css';
+import courses from './courses';
+
+class App extends Component {
+  render() {
+        const columns = [{
+            Header: 'Course',
+            accessor: 'Course',
+            maxWidth: 200,
+            filterAll: true
+        }, {
+            Header: 'Description',
+            accessor: 'Description',
+            width: 300,
+            filterAll: true,
+            style: {textAlign: 'left'}
+        }, {
+            Header: 'CR',
+            accessor: 'CR',
+            width: 50,
+            filterAll: true
+        }, {
+            Header: 'Offered',
+            accessor: 'Offered',
+            filterAll: true
+        }, {
+            Header: 'Prerequisites',
+            accessor: 'Prerequisites',
+            width: 300,
+            filterAll: true
+        }, {
+            Header: 'Minor',
+            accessor: 'Minor~',
+            width: 90,
+            filterAll: true
+        }, {
+            Header: 'CORE',
+            accessor: 'CORE',
+            width: 80,
+            filterAll: true
+        }, {
+            Header: 'Gen Ed',
+            accessor: 'Gen Ed',
+            filterAll: true
+        }, {
+            Header: 'Major',
+            accessor: 'Major',
+            width: 75,
+            filterAll: true
+        }];
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        Welcome
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </h1>
+                </header>
+                <div className="App-body">
+                    <h3>Course Table: </h3>
+                    <div id="course-table">
+                        <ReactTable
+                            data={courses}
+                            columns={columns}
+                            filterable={true}
+                            defaultFilterMethod={(filter, rows, column) =>
+                                matchSorter(rows, filter.value, { keys: [column.Header] })}
+                            className="-striped -highlight"
+                            />
+                    </div>
+                    <div id="table-info">
+                        <p style={{fontSize: '18px'}}><b>Notes:</b></p>
+                        <b>*Course approved for more than one category may be used only once to complete minor requirements.< br />
+                        *Please see the Sustainability Studies advisor to determine its best placement in your program. <br />
+                        ~Minor category abbreviations are as follows: SAT is Science and Technology; PAI is Policy and Institutions; SHD is Social and Human Dimensions <br />
+                        </b>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
